25b2a72e1974a8640fd32dad800dc81f86c96f4d
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2024-12-11 08:42:08 +0100

    .

--- ex02-matchers/test/strings.test.js
+++ ex02-matchers/test/strings.test.js
@@ -7,19 +7,19 @@ describe("strings.test", () => {
 
   it.todo("should verify that `myemail` ends with .eu")
 
-  it.todo("should verify that `myemail` does NOT with .com")
+  it.todo("should verify that `myemail` does NOT end with .com")
 
   it.todo("should verify that `myemail` includes 'binary'")
   it.todo(
-    "should verify that `myemail` includes 'andreas' and 'stars' (using only 1 matcher)"
+    "should verify that `myemail` includes both 'andreas' and 'stars' (using only 1 matcher)"
   )
 
   it.todo(
-    "should verify that `myemail` matches the regex `/[a-z.-]{1,}@[a-z-]{1,}.[a-z]{2,}/`"
+    "should verify that `myemail` matches the regex `/[a-z.-]{1,}@[a-z-]{1,}\\.[a-z]{2,}/`"
   )
 
   it.todo(
-    "should verify that `myemail` does NOT match the regex `/[a-z.-]{1,}@[a-z-]{1,}.[a-z]{3,}/`"
+    "should verify that `myemail` does NOT match the regex `/[a-z.-]{1,}@[a-z-]{1,}\\.[a-z]{3,}/`"
   )
 
   it.todo("should verify that `mycolor` is hexadecimal")
