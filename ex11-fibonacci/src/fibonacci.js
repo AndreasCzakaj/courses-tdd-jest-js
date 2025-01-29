d5024530aaf269af4991088e90edc1f34edf2a18
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2025-01-29 19:34:14 +0100

    fibonacci solution

--- ex11-fibonacci/src/fibonacci.js
+++ ex11-fibonacci/src/fibonacci.js
@@ -1,3 +1,39 @@
-export class Fibonacci {
-  calc() {}
+export class FibonacciLoopImpl {
+  calc(index) {
+    validateIndex(index)
+    if (index < 2) {
+      return index
+    }
+    let n2 = 0
+    let n1 = 1
+    let n = 0
+    for (let i = 1; i < index; i++) {
+      n = n1 + n2
+      n2 = n1
+      n1 = n
+    }
+    return n
+  }
+}
+
+export class FibonacciRecursionImpl {
+  calc(index) {
+    validateIndex(index)
+    if (index < 2) {
+      return index
+    }
+    return this.calc(index - 1) + this.calc(index - 2)
+  }
+}
+
+function validateIndex(index) {
+  if (
+    index === undefined ||
+    index === null ||
+    !Number.isInteger(index) ||
+    isNaN(index) ||
+    index < 0
+  ) {
+    throw new Error("index must be a number >= 0")
+  }
 }
--- ex11-fibonacci/test/fibonacci.test.js
+++ ex11-fibonacci/test/fibonacci.test.js
@@ -1,37 +1,37 @@
-import { Fibonacci } from "../src/fibonacci"
+import { FibonacciLoopImpl, FibonacciRecursionImpl } from "../src/fibonacci"
 
 export const fibonacciTest = (name, fibonacci) =>
   describe(`fibonacci test using implementation ${name}"`, () => {
-    it.todo("should throw an Error for undefined index")
-
-    it.todo("should throw an Error for null index")
-
-    it.todo("should throw an Error for negative index")
-
-    it.todo("should throw an Error for index > 46")
-
-    it.todo("should yield 0 for 0")
-
-    it.todo("should yield 1 for 1")
-
-    it.todo("should yield 1 for 2")
-
-    it.todo(
-      "by now, you should be thoroughly annoyed about all the repetitions, I hope..."
+    it.each`
+      index        | expected
+      ${undefined} | ${"index must be a number >= 0"}
+      ${null}      | ${"index must be a number >= 0"}
+      ${""}        | ${"index must be a number >= 0"}
+      ${"x"}       | ${"index must be a number >= 0"}
+      ${-1}        | ${"index must be a number >= 0"}
+    `(
+      "should fail for index $index with error $expected",
+      ({ index, expected }) => {
+        const action = () => fibonacci.calc(index)
+        expect(action).toThrow(expected)
+      }
     )
 
-    it.todo(`
-      index        | expected     | reason
-      ${0}         | ${0}         | ${" 0 => 0"}
-      ${1}         | ${1}         | ${" 1 => 1"}
-      ${2}         | ${1}         | ${" 2 => 1"}
-      ${3}         | ${2}         | ${" 3 => 2"}
-      ${5}         | ${5}         | ${" 5 => 5"}
-      ${6}         | ${8}         | ${" 6 => 8"}
-      ${7}         | ${13}        | ${" 7 => 13"}
-      ${19}        | ${4_181}     | ${" 19 => 4_181"}
-      ${30}        | ${832_040}   | ${" 30 => 832_040"}
-    `)
+    it.each`
+      index | expected
+      ${0}  | ${0}
+      ${1}  | ${1}
+      ${2}  | ${1}
+      ${3}  | ${2}
+      ${5}  | ${5}
+      ${6}  | ${8}
+      ${7}  | ${13}
+      ${19} | ${4_181}
+      ${30} | ${832_040}
+    `("should yield $expected for index $index", ({ index, expected }) => {
+      const actual = fibonacci.calc(index)
+      expect(actual).toEqual(expected)
+    })
 
     it.skip("should show which impl is slow af", () => {
       expect(fibonacci.calc(46)).toBe(1_836_311_903)
@@ -39,5 +39,9 @@ export const fibonacciTest = (name, fibonacci) =>
   })
 
 describe("Fibonacci 1st implementation", () => {
-  fibonacciTest("1st impl", new Fibonacci())
+  fibonacciTest("1st impl", new FibonacciLoopImpl())
+})
+
+describe("Fibonacci 2nd implementation", () => {
+  fibonacciTest("2nd impl", new FibonacciRecursionImpl())
 })
